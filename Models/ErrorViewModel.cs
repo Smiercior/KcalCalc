8c8e17608685cee0832abc03c018dd21dbc7248a
Smiercior <smiercior@gmail.com>
2023-04-19 23:19:01 +0200

    create app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KcalCalc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
